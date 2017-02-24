8d341fa02419312225da6a620cbe0103f099b3b7
Camden Segal <camden.segal@gmail.com>
2017-02-24 17:46:41 -0600

    Flower adaptations for ragespline

---
+++ Assets/Scripts/FlowerColorer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowerColorer : MonoBehaviour {
+	RageSpline[] splines;
+
+	void Start() {
+		splines = GetComponentsInChildren<RageSpline>();
+	}
+
+	public void SetColor(Color c) {
+		if (splines == null) {
+			Start();
+		}
+		foreach (var spline in splines) {
+			spline.SetFillColor2(c);
+		}
+	}
+}
