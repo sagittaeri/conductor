7b079e3bc6a5bfe3c8d5684b3a7c2b25376cc57c
Jake OBrien <neuronrain@gmail.com>
2017-02-26 09:39:18 -0800

    glowy thing behaviour

---
+++ Assets/Scripts/Jelly.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class Jelly : MonoBehaviour 
+{
+
+    public Rect targetRect;
+    public Flowchart flowchart;
+
+    public void SetRandomDestination()
+    {
+        var target = new Vector3(Random.Range(targetRect.xMin, targetRect.xMax), Random.Range(targetRect.yMin, targetRect.yMax), transform.position.z);
+        var v = flowchart.GetVariable<VariableBase<Vector3>>("jellyTarget");
+        v.Value = target;
+    }
+
+
+}
